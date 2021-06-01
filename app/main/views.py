c340c2663f39d2da9a2db5908f970116a023cd4c
lyatuu <daniellyatuu@gmail.com>
2021-06-01 21:23:16 +0300

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,50 @@
+from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+import time
+import os
+
+class SpeekView(View):
+    def get(self, request, *args, **kwargs):
+
+        text = 'Hello Ephrasia'
+
+        tts = gTTS(text=text, lang='en')
+        filename = 'voice.mp3'
+        file_location = 'voice/'+filename
+        tts.save(file_location)
+        playsound.playsound(file_location)
+
+        return HttpResponse('here please')
+
+class GetAudioView(View):
+    def speek(self, text, name):
+        tts = gTTS(text=text, lang='en')
+        file_location = 'voice/'+name
+        tts.save(file_location)
+        playsound.playsound(file_location)
+
+    def get(self, request, *args, **kwargs):
+
+        # NOTIFY USER TO SPEECH
+        new_name = 'voice_'+str(time.time()).replace('.', '')+'.mp3'
+        self.speek('Please Speek', new_name)
+
+        r = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+            said = ''
+
+            try:
+                said = r.recognize_google(audio)
+                print(said)
+                return HttpResponse(said)
+            except Exception as e:
+                print('error', e)
+                return HttpResponse(e)
