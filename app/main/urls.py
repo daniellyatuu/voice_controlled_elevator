316b46813d672d20bb9c092a8561409b19135d88
lyatuu <daniellyatuu1@gmail.com>
2021-08-04 11:53:59 +0300

    update

--- app/main/urls.py
+++ app/main/urls.py
@@ -6,6 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('speek/', views.SpeekView.as_view(), name='speek_view'),
     path('get-audio/', views.GetAudioView.as_view(), name='get_audio_view'),
-    path('get-audio/', views.GetAudioView.as_view(), name='get_audio_view'),
+    path('check-port/', views.CheckSerialPort.as_view(), name='check_port'),
+    path('control-port/', views.ControlSerialPort.as_view(), name='control_port'),
     path('home/', views.HomeView.as_view(), name='home'),
 ]
